adfa7c999068acbeae215363629ff2d6306aa855
Henryk Plötz <henryk@ploetzli.ch>
2017-07-17 22:37:49 +0200

    Work towards letsencrypt handler support

--- filter_plugins/filters.py
+++ filter_plugins/filters.py
@@ -1,7 +1,12 @@
-def default_hash(data, defaults):
+def default_hash(data, defaults, key=None):
 	"Reverse combine. Returns a copy of data with all keys that are set in defaults but not in data set to the value from defaults."
-	retval = dict( defaults )
-	retval.update(data)
+	if key is None:
+		retval = dict( defaults )
+		retval.update(data)
+	else:
+		retval = dict(data)
+		retval[key] = dict(defaults)
+		retval[key].update(data.get(key, {}))
 	return retval
 
 class FilterModule(object):
