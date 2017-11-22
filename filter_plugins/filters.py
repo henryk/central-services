9cbf85ad2b2e64c66f57391b44e042f6567b0862
Henryk Plötz <henryk@ploetzli.ch>
2017-11-22 02:34:27 +0100

    Work toward network hooks and port forwarding. (Work around an extremely annoying bug/circular reference.)

--- filter_plugins/filters.py
+++ filter_plugins/filters.py
@@ -9,6 +9,16 @@ def default_hash(data, defaults, key=None):
 		retval[key].update(data.get(key, {}))
 	return retval
 
+def select_keys(data, keys):
+	"Return a copy of data without everything not in keys. If data argument is list, apply to the subelements."
+	if isinstance(data, (list, tuple)):
+		return [select_keys(e, keys) for e in data]
+	else:
+		retval = dict()
+		for key in set( data.keys() ).intersection( set(keys) ):
+			retval[key] = data[key]
+		return retval
+
 class FilterModule(object):
 	'''
 	custom jinja2 filters
@@ -16,5 +26,6 @@ class FilterModule(object):
 
 	def filters(self):
 		return {
-			'default_hash': default_hash
+			'default_hash': default_hash,
+			'select_keys': select_keys
 		}
